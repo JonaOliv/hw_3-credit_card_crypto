ba95a9afbfd8f4df9e0262f730167cf5329e737e
Jonathan <jonathanolivataiwan@gmail.com>
2019-03-14 16:09:04 +0800

    First commit

--- credit_card.rb
+++ credit_card.rb
@@ -3,18 +3,27 @@ require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
+  include LuhnValidator
 
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      :number => @number,
+      :expiration_date => @expiration_date,
+      :owner => @owner,
+      :credit_network => @credit_network
     }.to_json
   end
 
@@ -26,6 +35,8 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    card_a = JSON.parse(card_s)
+    initialize("number", "expiration_date", "owner", "credit_network")
   end
 
   # return a hash of the serialized credit card object
@@ -34,6 +45,7 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    JSON.parse(to_s)
   end
 
   # return a cryptographically secure hash
@@ -41,5 +53,6 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 from openssl to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    OpenSSL::Digest.digest("SHA256", to_s)
   end
 end
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,23 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    sum_anwser = 0
+    bool_alternative = false
+
+    nums_a.reverse.each do |i|
+        if (bool_alternative)
+          if ((i * 2) > 9)
+            sum_anwser = sum_anwser + (((i * 2) % 10) + 1)
+          else
+            sum_anwser = sum_anwser + ( i * 2)
+          end
+        else
+          sum_anwser = sum_anwser + i
+        end
+        bool_alternative = !bool_alternative
+
+    end
+    (sum_anwser % 10 == 0)
+
   end
 end
