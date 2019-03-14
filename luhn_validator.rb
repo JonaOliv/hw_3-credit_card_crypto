0737fd0b0d2e58241d005ae8d0e7411b63d6a8a3
Jonathan <jonathanolivataiwan@gmail.com>
2019-03-14 23:16:35 +0800

    the less heavy after 25 times

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -10,47 +10,9 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
-    # number = "376190711495296 "
-    # nums_a = number.to_s.chars.map(&:to_i)
-    boolen = true
 
-    nums_a.reverse!.map! do |i|
-      boolen = !boolen
-      boolen ? i * 2 : i
-    end
-    # nums_a.reverse_each { |i, index| i * 2 if index.even? }
-
-    # nums_a.map! { |i| i % 10 + 1 if i > 9 }
-    # ((nums_a.reduce { |n1, n2| n1 + n2 }) % 10).zero?
-
-    nums_a = nums_a.reduce { |n1, n2| n2 > 9 ? n2 % 10 + 1 + n1 : n1 + n2 }
-
-    # nums_a.reverse!.reduce! do |n1, n2, i|
-    #   if i.even?
-    #     n2 * 2 > 9 ? (n2 * 2) % 10 + 1 + n1 : n1 + n2 * 2
-    #   else
-    #     n1 + n2
-    #   end
-    # end
-
-    # nums_a.reverse!.reduce! do |n1, n2| n2 > 9 ? n2 % 10 + 1 + n1 : n1 + n2
-    #   if
-    #
-    #   else
-    #
-    #   end
-    # end
-
-    (nums_a % 10).zero?
-    # ((nums_a.reduce { |n1, n2| n1 + n2 }) % 10).zero?
-
-    # (nums_a.reverse.reduce do |n1, n2|
-    #   b_jumping = !b_jumping
-    #   if b_jumping
-    #     (n2 * 2) > 9 ? n1 + (n2 * 2) % 10 + 1 : n1 + (n2 * 2)
-    #   else
-    #     n1 + n2
-    #   end
-    # end % 10).zero?
+    (nums_a.reverse.each_slice(2).flat_map do |odd, even|
+      [odd.to_i, *(even.to_i * 2).divmod(10)]
+    end.inject(:+) % 10).zero?
   end
 end
