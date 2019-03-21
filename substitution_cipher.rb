cb9516c83a5f4cc4a65df77a7f2bd427ca564cd8
esamgel <esamgel@gmail.com>
2019-03-21 18:23:19 +0800

    more rubocop issue resolve

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -25,29 +25,29 @@ module SubstitutionCipher
     #   document: String
     #   key: Fixnum (integer)
     # Returns: String
- 
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
 
-      # convert obj to json string
+      # convert document(creditcard obj) to json string
       document = Permutation.to_json_string(document)
 
-      # generate lookup table
+      # generate a lookup table
       lookup_tbl = generate_lookup_tbl
 
       # convert the document to int representation using the lookup_tbl
-      org_doc_fixnum = []
-      document.split('').each { |k| org_doc_fixnum.push(lookup_tbl[k]) }
+      doc_fixnum = []
+      document.split('').each { |k| doc_fixnum.push(lookup_tbl[k]) }
 
-      # generate a shuffle value lookup table
-      shuff_lookup_tbl = Permutation.shuffle_lookup_tbl(lookup_tbl,key)
+      # shuffle the values in the lookup table using the key
+      shuff_lookup_tbl = Permutation.shuffle_lookup_tbl(lookup_tbl, key)
 
       # convert the org_doc_fixnum to string using the suffled lookup_tbl
       cipher_string = []
-      org_doc_fixnum.each { |val| cipher_string.push(shuff_lookup_tbl.key(val.to_i)) }
-      cipher_string = cipher_string.join('')
+      doc_fixnum.each do |val|
+        cipher_string.push(shuff_lookup_tbl.key(val.to_i))
+      end
 
-      return cipher_string
+      cipher_string.join('')
     end
 
     # Decrypts String document using integer key
@@ -55,15 +55,15 @@ module SubstitutionCipher
     #   document: String
     #   key: Fixnum (integer)
     # Returns: String
-    # returns json string    
+    # returns json string
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
 
-      # generate lookup table
+      # generate a lookup table
       lookup_tbl = generate_lookup_tbl
 
-      # generate a shuffle value lookup table
-      shuff_lookup_tbl = Permutation.shuffle_lookup_tbl(lookup_tbl,key)
+      # shuffle the values in the lookup table using the key
+      shuff_lookup_tbl = Permutation.shuffle_lookup_tbl(lookup_tbl, key)
 
       # convert the document to int representation using the shuff_lookup_tbl
       cipher_fixnum = []
@@ -71,16 +71,19 @@ module SubstitutionCipher
 
       # convert the cipher_fixnum to string using the lookup_tbl
       decipher_string = []
-      cipher_fixnum.each { |val| decipher_string.push(lookup_tbl.key(val.to_i)) }
+      cipher_fixnum.each do |val|
+        decipher_string.push(lookup_tbl.key(val.to_i))
+      end
       decipher_string = decipher_string.join('')
 
-      return decipher_string
+      decipher_string
     end
 
     # convert document to json_string
     def self.to_json_string(c_card)
       {
-        # TODO: setup the hash with all instance vairables to serialize into json
+        # TODO: setup the hash with all instance
+        #  vairables to serialize into json
         "number": c_card.number,
         "expiration_date": c_card.expiration_date,
         "owner": c_card.owner,
@@ -91,32 +94,32 @@ module SubstitutionCipher
     # create/generate a lookup table
     def self.generate_lookup_tbl
       lookup_tbl = {}
-      h_key = ('a'..'z').to_a + (('A'..'Z').to_a) + (('!'..'?').to_a) + (['{','}',' ','_'])
+      h_key = ('a'..'z').to_a + ('A'..'Z').to_a + ('!'..'?').to_a
+      h_key += ['{', '}', ' ', '_']
       h_val = (0..127).to_a
       v = 0
-
       h_key.each do |k|
         lookup_tbl[k] = h_val[v]
         v += 1
       end
-      return lookup_tbl
+      lookup_tbl
     end
 
     # shuffle the values of a lookup table
-    def self.shuffle_lookup_tbl(lookup_tbl,key)
+    def self.shuffle_lookup_tbl(lookup_tbl, key)
       shuff_lookup_tbl = {}
       max_val = 0
-      lookup_tbl.max_by{|k,v| max_val = v}
-      h_key = ('a'..'z').to_a + (('A'..'Z').to_a) + (('!'..'?').to_a) + (['{','}',' ','_'])
-      h_val2 =(0..max_val).to_a
-      h_val2 = h_val2.shuffle(random: Random.new(key)) # shuffle values using key
+      lookup_tbl.max_by { |_, v| max_val = v }
+      h_key = ('a'..'z').to_a + ('A'..'Z').to_a + ('!'..'?').to_a
+      h_key += ['{', '}', ' ', '_']
+      h_val2 = (0..max_val).to_a
+      h_val2 = h_val2.shuffle(random: Random.new(key)) # shuffle values
       v = 0
-
-      h_key.each do |k|
+      h_key.map do |k|
         shuff_lookup_tbl[k] = h_val2[v]
         v += 1
       end
-      return shuff_lookup_tbl
+      shuff_lookup_tbl
     end
   end
 end
