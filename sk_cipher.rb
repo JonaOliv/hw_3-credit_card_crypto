949741f0c85949d7eb8511d8a9d2b0adbf9076e6
Jonathan <jonathanolivataiwan@gmail.com>
2019-03-28 15:20:01 +0800

    encrypt

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -5,11 +5,16 @@ require 'base64'
 module ModernSymmetricCipher
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
+    key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    Base64.strict_encode64(key)
   end
 
   def self.encrypt(document, key)
     # TODO: Return an encrypted string
     #       Use base64 for ciphertext so that it is sendable as text
+    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
+    ciphertext = simple_box.encrypt(document)
+    Base64.strict_encode64(ciphertext)
   end
 
   def self.decrypt(encrypted_cc, key)
