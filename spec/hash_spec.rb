398c74422b9e4c44cca2fde4506fca0b72fb0191
esamgel <esamgel@gmail.com>
2019-03-29 00:12:09 +0800

    Rubocop error reduced

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -23,25 +23,24 @@ cards = card_details.map do |c|
   CreditCard.new(c[:num], c[:exp], c[:name], c[:net])
 end
 
-
 describe 'Test hashing requirements' do
   before do
     class << self
       %w[hash_secure hash].each do |action|
-        define_method("unique_#{action}") do |cards,name|
-          cards.each do |curr_card|
+        define_method("unique_#{action}") do |cards, n|
+          cards.each do |curr|
             0.upto(cards.size - 1) do |x|
-              curr_card != cards[x] ? (curr_card.send(name).wont_equal cards[x].send(name)) : ()
+              curr.send(n).wont_equal cards[x].send(n) if curr != cards[x]
             end
-          end   
+          end
         end
 
-        define_method("consistent_#{action}") do |cards, name|
+        define_method("const_#{action}") do |cards, n|
           cards.each do |card|
             first_hash = {}
             count = 1
             1.upto(10) do # repeated hash card obj 10 times
-              repeated_hash = card.send(name)
+              repeated_hash = card.send(n)
               if count > 0
                 first_hash = repeated_hash
                 count -= 1
@@ -50,50 +49,48 @@ describe 'Test hashing requirements' do
             end
           end
         end
-      
       end
     end
   end
 
-    describe 'Test regular hashing' do
-      describe 'Check hashes are consistently produced' do
-        # TODO: Check that each card produces the same hash if hashed repeatedly
-        it 'Test card if its hashed repeatedly 10 times but still consistent' do
-          consistent_hash(cards,'hash')
-        end
+  describe 'Test regular hashing' do
+    describe 'Check hashes are consistently produced' do
+      # TODO: Check that each card produces the same hash if hashed repeatedly
+      it 'Test card if its hashed repeatedly 10 times but still consistent' do
+        const_hash(cards, 'hash')
       end
     end
+  end
 
-    describe 'Check for unique hashes' do
+  describe 'Check for unique hashes' do
     # TODO: Check that each card produces a different hash than other cards
-      it 'Test if each card produce unigue hashes from each other' do
-        #define_method("unique_#{action}") do |cards, cipher:|
-        unique_hash(cards,"hash")
+    it 'Test if each card produce unigue hashes from each other' do
+      unique_hash(cards, 'hash')
+    end
+  end
+
+  describe 'Test cryptographic hashing' do
+    describe 'Check hashes are consistently produced' do
+      # TODO: Check that each card produces the same hash if hashed repeatedly
+      it 'Test card if its hashed repeatedly 10 times but still consistent' do
+        const_hash(cards, 'hash_secure')
       end
     end
+  end
 
-    describe 'Test cryptographic hashing' do
-      describe 'Check hashes are consistently produced' do
-        # TODO: Check that each card produces the same hash if hashed repeatedly
-        it 'Test card if its hashed repeatedly 10 times but still consistent' do
-          consistent_hash(cards,'hash_secure')
-      end
+  describe 'Check for unique hashes' do
+    # TODO: Check that each card produces a different hash than other cards
+    it 'Test if each card produce unigue hashes from each other' do
+      unique_hash(cards, 'hash_secure')
     end
+  end
 
-    describe 'Check for unique hashes' do
-      # TODO: Check that each card produces a different hash than other cards
-      it 'Test if each card produce unigue hashes from each other' do
-        unique_hash(cards,"hash_secure")
+  describe 'Check regular hash not same as cryptographic hash' do
+    # TODO: Check that each card's hash is different from its hash_secure
+    it 'Test if each card hash different from their hash_secure' do
+      cards.each do |c|
+        c.hash.wont_equal c.hash_secure
       end
-    end
-
-    describe 'Check regular hash not same as cryptographic hash' do
-      # TODO: Check that each card's hash is different from its hash_secure
-      it 'Test if each card hash different from their hash_secure' do
-        cards.each do |c|
-          c.hash.wont_equal c.hash_secure
-        end
-      end    
     end
   end
 end
